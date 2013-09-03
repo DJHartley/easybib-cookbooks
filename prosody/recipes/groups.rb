06b3e935606c1c14f5c89d0e0d1f5a6d92c69be9
till <till@php.net>
2013-09-03 16:50:51 +0200

    Bugfix: always write groups (so we can update it)

--- prosody/recipes/groups.rb
+++ prosody/recipes/groups.rb
@@ -1,10 +1,7 @@
 groups_cfg = "/etc/prosody/conf.d/prosody_groups.lua"
 
 execute "load sharedgroups" do
-  command "echo 'groups_file = \"/var/prosody/sharedgroups.txt\"' >> #{groups_cfg}"
-  not_if do
-    File.exists?(groups_cfg)
-  end
+  command "echo 'groups_file = \"/var/prosody/sharedgroups.txt\"' > #{groups_cfg}"
 end
 
 template "/var/prosody/sharedgroups.txt" do
